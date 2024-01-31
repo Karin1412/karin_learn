c5287d11609fcc37cfc786fa589fd379dc2aac4d
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-01-31 21:58:32 +0700

    add

---
+++ destructure.js
@@ -0,0 +1,12 @@
+const sv = {
+    name: 'Karin',
+    age: '21'
+}
+
+const {name, age} = sv
+
+const myName = name
+const myAge = age
+
+console.log(`My name is ${myName}`)
+console.log(`I am ${myAge} years old`)
